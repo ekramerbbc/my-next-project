e3f7ca0b61a33a15870365a676c5bb008bf6bce5
ekramerbbc <ekramer@bluebottlecoffee.com>
2020-12-15 14:32:33 -0500

    add name query

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
+
+export const ALL_PREPARATION_GUIDES_QUERY = gql`
+  query allPreparationGuides {
+    allPreparationGuide {
+      _id
+      name
+    }
+  }
+`;
+
 function Home() {
-  return <div>Home</div>
+  const { loading, error, data } = useQuery(ALL_PREPARATION_GUIDES_QUERY);
+
+  if (loading) return <>Loading...</>
+
+  if (error) return <>`Error! ${error.message}`</>
+
+  const { allPreparationGuide: allPreparationGuides } = data;
+
+  return (
+    <div>
+      {allPreparationGuides.map(preparationGuide => {
+        return (
+          <div>
+            <div>{preparationGuide.name}</div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Home
